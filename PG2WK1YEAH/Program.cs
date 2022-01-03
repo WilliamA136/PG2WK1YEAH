df4022a32b5798aa45ca265a610d19d5a53ae695
William Allen <WMAllen@student.fullsail.edu>
2022-01-03 10:05:31 -0500

    Add project files.

---
+++ PG2WK1YEAH/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PG2WK1YEAH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
